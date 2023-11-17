1368af3a0dffa85a5af4cf638b34503f11a5d60f
BelkaAlex01 <Alex01020304045@mail.ru>
2023-11-17 20:47:25 +0300

    add checker.h file

---
+++ bracketChecker/checker.h
@@ -0,0 +1,23 @@
+#include <string>
+
+using namespace std;
+
+bool bracketChecker(string s){
+    int len = s.length();
+    int cnt = 0;
+    char last = '0';
+    for(int i = 0; i<len;i++){
+        if(s[i]=='('){
+            cnt++; last = '(';
+        }
+        else if( s[i]==')'){
+            if(cnt){
+                cnt--;
+                if(last !='(' && last!=')') return false;
+            }
+            else return false;
+            last = ')';
+        }
+    }
+    return true;
+}
--- bracketChecker/main.cpp
+++ bracketChecker/main.cpp
@@ -1,27 +1,8 @@
 #include <iostream>
 #include <string>
+#include "checker.h"
 
 using namespace std;
-
-bool bracketChecker(string s){
-    int len = s.length();
-    int cnt = 0;
-    char last = '0';
-    for(int i = 0; i<len;i++){
-        if(s[i]=='('){
-            cnt++; last = '(';
-        }
-        else if( s[i]==')'){
-            if(cnt){
-                cnt--;
-                if(last !='(' && last!=')') return false;
-            }
-            else return false;
-            last = ')';
-        }
-    }
-    return true;
-}
 
 int main()
 {
